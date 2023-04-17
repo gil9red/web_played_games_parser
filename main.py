9d44d59d5f89ff1ad6e362bb77bac7dbca82c28a
gil9red <ilya.petrash@inbox.ru>
2023-04-17 18:10:32 +0500

    Рефакторинг

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import json
@@ -17,18 +17,18 @@ from third_party.mini_played_games_parser import get_played_games
 
 
 DIR = Path(__file__).resolve().parent
-PATH_USERS = DIR / 'users.json'
+PATH_USERS = DIR / "users.json"
 
 # Example:
 # {
 #     "<LOGIN>": "<PASSWORD>"
 # }
-users = json.loads(PATH_USERS.read_text('utf-8'))
+users = json.loads(PATH_USERS.read_text("utf-8"))
 
 
 app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False
-app.config['SECRET_KEY'] = '<SECRET_KEY_HERE>'
+app.config["JSON_SORT_KEYS"] = False
+app.config["SECRET_KEY"] = "<SECRET_KEY_HERE>"
 
 auth = HTTPDigestAuth()
 
@@ -41,10 +41,7 @@ def get_password(username: str) -> str | None:
 @app.route("/")
 @auth.login_required
 def index():
-    return render_template(
-        "index.html",
-        title='Парсер игр'
-    )
+    return render_template("index.html", title="Парсер игр")
 
 
 @app.route("/get_played_games")
@@ -52,9 +49,11 @@ def played_games():
     return jsonify(get_played_games())
 
 
-if __name__ == '__main__':
-    host = '0.0.0.0'
+if __name__ == "__main__":
+    host = "0.0.0.0"
     port = 10008
-    print(f"HTTP server running on http://{'127.0.0.1' if host == '0.0.0.0' else host}:{port}")
+    print(
+        f"HTTP server running on http://{'127.0.0.1' if host == '0.0.0.0' else host}:{port}"
+    )
 
     app.run(host=host, port=port)
